7885a99194522abaf673d3ddf908a782f1c477ae
Ying Huang <sindica2000@yahoo.com>
2019-10-31 17:46:07 -0700

    Add controller framework and replicaset controller

---
+++ pkg/cloudfabric-controller/controllerframework/cim_test.go
@@ -0,0 +1,296 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllerframework
+
+import (
+	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes/fake"
+	"testing"
+)
+
+var alwaysReady = func() bool { return true }
+var notifyTimes int
+
+func createControllerInstanceManager(stopCh chan struct{}, updateCh chan string) (*ControllerInstanceManager, informers.SharedInformerFactory) {
+	client := fake.NewSimpleClientset()
+	informers := informers.NewSharedInformerFactory(client, 0)
+
+	cim := NewControllerInstanceManager(informers.Core().V1().ControllerInstances(), client, updateCh)
+	go cim.Run(stopCh)
+
+	cim.controllerListerSynced = alwaysReady
+	cim.notifyHandler = mockNotifyHander
+	checkInstanceHandler = mockCheckInstanceHander
+	return GetControllerInstanceManager(), informers
+}
+
+func newControllerInstance(controllerType string, controllerKey int64, workloadNum int32, isLocked bool) *v1.ControllerInstance {
+	controllerInstance := &v1.ControllerInstance{
+		ObjectMeta: metav1.ObjectMeta{
+			ResourceVersion: "100",
+		},
+		ControllerType: controllerType,
+		ControllerKey:  controllerKey,
+		WorkloadNum:    workloadNum,
+		IsLocked:       isLocked,
+	}
+
+	controllerInstance.Name = generateControllerName()
+
+	return controllerInstance
+}
+
+func mockNotifyHander(controllerInstance *v1.ControllerInstance) {
+	notifyTimes++
+	return
+}
+
+func mockCheckInstanceHander() {
+	return
+}
+
+func testAddEvent(t *testing.T, cim *ControllerInstanceManager, notifyTimes int) (*v1.ControllerInstance, string, map[string]v1.ControllerInstance) {
+	// add event
+	controllerType := "foo"
+	controllerInstance1 := newControllerInstance(controllerType, 10000, 999, false)
+	cim.addControllerInstance(controllerInstance1)
+
+	controllerInstanceMap, err := cim.ListControllerInstances(controllerType)
+	assert.NotNil(t, controllerInstanceMap)
+	assert.Nil(t, err)
+	controllerInstanceRead, isOK := controllerInstanceMap[controllerInstance1.Name]
+	assert.True(t, isOK)
+	assert.NotNil(t, controllerInstanceRead)
+	assert.Equal(t, controllerInstance1.Name, controllerInstanceRead.Name)
+	assert.Equal(t, controllerInstance1.ControllerType, controllerInstanceRead.ControllerType)
+	assert.Equal(t, controllerInstance1.ControllerKey, controllerInstanceRead.ControllerKey)
+	assert.Equal(t, controllerInstance1.WorkloadNum, controllerInstanceRead.WorkloadNum)
+	assert.Equal(t, controllerInstance1.IsLocked, controllerInstanceRead.IsLocked)
+	assert.Equal(t, notifyTimes, notifyTimes, "Unexpected notify times")
+
+	return &controllerInstanceRead, controllerType, controllerInstanceMap
+}
+
+func TestSyncControllerInstances_Nil(t *testing.T) {
+	stopCh := make(chan struct{})
+	updateCh := make(chan string)
+	defer close(stopCh)
+	defer close(updateCh)
+
+	cim, _ := createControllerInstanceManager(stopCh, updateCh)
+	assert.True(t, cim.isControllerListInitialized, "Expect controller list is initialized")
+
+	// invalid controller type
+	controllerType := "foo"
+	controllerInstanceMap, err := cim.ListControllerInstances(controllerType)
+	assert.Nil(t, controllerInstanceMap, "Expecting nil controller map for controller type not in map")
+	assert.Nil(t, err, "Expecting no error for not existed controller")
+}
+
+func TestControllerInstancesAddAndUpdateEventHandler(t *testing.T) {
+	stopCh := make(chan struct{})
+	updateCh := make(chan string)
+	defer close(stopCh)
+	defer close(updateCh)
+
+	cim, _ := createControllerInstanceManager(stopCh, updateCh)
+	notifyTimes = 0
+
+	// add event
+	controllerInstance1, controllerType, controllerInstanceMap := testAddEvent(t, cim, 1)
+
+	// update event
+	controllerInstance2 := controllerInstance1.DeepCopy()
+	controllerInstance2.IsLocked = !controllerInstance1.IsLocked
+	controllerInstance2.WorkloadNum = controllerInstance1.WorkloadNum + 101
+	controllerInstance2.ControllerKey = controllerInstance1.ControllerKey - 100
+	controllerInstance2.ResourceVersion = "101"
+	cim.updateControllerInstance(controllerInstance1, controllerInstance2)
+
+	controllerInstanceMapNew, err := cim.ListControllerInstances(controllerType)
+	assert.NotNil(t, controllerInstanceMapNew)
+	assert.Nil(t, err)
+	assert.Equal(t, len(controllerInstanceMap), len(controllerInstanceMapNew), "Unexpected length of controller instance map")
+	controllerInstanceRead2, isOK := controllerInstanceMapNew[controllerInstance1.Name]
+	assert.True(t, isOK)
+	assert.NotNil(t, controllerInstanceRead2)
+	assert.Equal(t, controllerInstance1.Name, controllerInstanceRead2.Name)
+	assert.Equal(t, controllerInstance1.ControllerType, controllerInstanceRead2.ControllerType)
+	assert.NotEqual(t, controllerInstance1.IsLocked, controllerInstanceRead2.IsLocked)
+	assert.Equal(t, controllerInstance1.WorkloadNum+101, controllerInstanceRead2.WorkloadNum)
+	assert.Equal(t, controllerInstance1.ControllerKey-100, controllerInstanceRead2.ControllerKey)
+	assert.Equal(t, 2, notifyTimes, "Unexpected notify times")
+}
+
+func TestControllerInstanceDeleteEventHandler(t *testing.T) {
+	stopCh := make(chan struct{})
+	updateCh := make(chan string)
+	defer close(stopCh)
+	defer close(updateCh)
+
+	cim, _ := createControllerInstanceManager(stopCh, updateCh)
+	notifyTimes = 0
+
+	// add event
+	controllerInstance1, controllerType, _ := testAddEvent(t, cim, 1)
+
+	// delete event
+	cim.deleteControllerInstance(controllerInstance1)
+	controllerInstanceMapAfterDelete, err := cim.ListControllerInstances(controllerType)
+	assert.NotNil(t, controllerInstanceMapAfterDelete)
+	assert.Nil(t, err)
+	_, isOK := controllerInstanceMapAfterDelete[controllerInstance1.Name]
+	assert.False(t, isOK)
+	assert.Equal(t, 2, notifyTimes, "Unexpected notify times")
+}
+
+func TestDeletedControllerInstanceSentToAddEventHandler(t *testing.T) {
+	stopCh := make(chan struct{})
+	updateCh := make(chan string)
+	defer close(stopCh)
+	defer close(updateCh)
+
+	cim, _ := createControllerInstanceManager(stopCh, updateCh)
+	notifyTimes = 0
+
+	// add event
+	controllerInstance1, controllerType, _ := testAddEvent(t, cim, 1)
+
+	controllerInstance2 := controllerInstance1.DeepCopy()
+	now := metav1.Now()
+	controllerInstance2.DeletionTimestamp = &now
+	cim.addControllerInstance(controllerInstance2)
+	controllerInstanceMapNew, err := cim.ListControllerInstances(controllerType)
+
+	assert.NotNil(t, controllerInstanceMapNew)
+	assert.Nil(t, err)
+	assert.Equal(t, 0, len(controllerInstanceMapNew))
+	assert.Equal(t, 2, notifyTimes, "Unexpected notify times")
+}
+
+func TestDeleteControllerInstanceDoesNotExist(t *testing.T) {
+	stopCh := make(chan struct{})
+	updateCh := make(chan string)
+	defer close(stopCh)
+	defer close(updateCh)
+
+	cim, _ := createControllerInstanceManager(stopCh, updateCh)
+	notifyTimes = 0
+
+	controllerInstance1 := newControllerInstance("bar", 10000, 999, false)
+	cim.deleteControllerInstance(controllerInstance1)
+
+	controllerInstanceMap, err := cim.ListControllerInstances(controllerInstance1.ControllerType)
+	assert.Nil(t, controllerInstanceMap)
+	assert.Nil(t, err)
+	assert.Equal(t, 0, notifyTimes, "Unexpected notify times")
+}
+
+func TestAddMultipleControllerInstancesForSameControllerType(t *testing.T) {
+	stopCh := make(chan struct{})
+	updateCh := make(chan string)
+	defer close(stopCh)
+	defer close(updateCh)
+
+	cim, _ := createControllerInstanceManager(stopCh, updateCh)
+	notifyTimes = 0
+
+	// add event
+	controllerInstance1, controllerType1, _ := testAddEvent(t, cim, 1)
+	controllerInstance2, controllerType2, _ := testAddEvent(t, cim, 2)
+	assert.Equal(t, controllerType1, controllerType2)
+	assert.NotEqual(t, controllerInstance1.Name, controllerInstance2.Name)
+
+	controllerInstanceMap, err := cim.ListControllerInstances(controllerType1)
+	assert.Nil(t, err)
+	assert.NotNil(t, controllerInstanceMap)
+	controllerInstanceRead1, isOK1 := controllerInstanceMap[controllerInstance1.Name]
+	assert.True(t, isOK1)
+	assert.NotNil(t, controllerInstanceRead1)
+	assert.Equal(t, controllerInstance1.Name, controllerInstanceRead1.Name)
+
+	controllerInstanceRead2, isOK2 := controllerInstanceMap[controllerInstance2.Name]
+	assert.True(t, isOK2)
+	assert.NotNil(t, controllerInstanceRead2)
+	assert.Equal(t, controllerInstance2.Name, controllerInstanceRead2.Name)
+}
+
+func TestUpdateHandlerWithOldEvents(t *testing.T) {
+	stopCh := make(chan struct{})
+	updateCh := make(chan string)
+	defer close(stopCh)
+	defer close(updateCh)
+
+	cim, _ := createControllerInstanceManager(stopCh, updateCh)
+	notifyTimes = 0
+
+	// add event
+	controllerInstance1, controllerType, _ := testAddEvent(t, cim, 1)
+
+	// update event
+	controllerInstance2 := controllerInstance1.DeepCopy()
+	controllerInstance2.IsLocked = !controllerInstance1.IsLocked
+	controllerInstance2.WorkloadNum = controllerInstance1.WorkloadNum + 101
+	controllerInstance2.ControllerKey = controllerInstance1.ControllerKey - 100
+	controllerInstance2.ResourceVersion = "99"
+	cim.updateControllerInstance(controllerInstance1, controllerInstance2)
+
+	// check controller instance in the map
+	controllerInstanceMap2, err := cim.ListControllerInstances(controllerType)
+	assert.Nil(t, err)
+	assert.NotNil(t, controllerInstanceMap2)
+	controllerInstanceRead2, isOK := controllerInstanceMap2[controllerInstance1.Name]
+	assert.True(t, isOK)
+	assert.NotNil(t, controllerInstanceRead2)
+
+	assert.Equal(t, controllerInstance1.Name, controllerInstanceRead2.Name)
+	assert.Equal(t, controllerInstance1.ResourceVersion, controllerInstanceRead2.ResourceVersion)
+	assert.Equal(t, controllerInstance1.IsLocked, controllerInstanceRead2.IsLocked)
+	assert.Equal(t, controllerInstance1.WorkloadNum, controllerInstanceRead2.WorkloadNum)
+	assert.Equal(t, controllerInstance1.ControllerKey, controllerInstanceRead2.ControllerKey)
+}
+
+func TestErrorHandlingInListControllerInstances(t *testing.T) {
+	stopCh := make(chan struct{})
+	updateCh := make(chan string)
+	defer close(stopCh)
+	defer close(updateCh)
+
+	cim, _ := createControllerInstanceManager(stopCh, updateCh)
+	notifyTimes = 0
+
+	_, controllerType, _ := testAddEvent(t, cim, 1)
+	testAddEvent(t, cim, 2)
+
+	controllerInstance3 := newControllerInstance("foo2", 10000, 999, false)
+	cim.addControllerInstance(controllerInstance3)
+
+	cim.isControllerListInitialized = false
+
+	controllerInstanceMap1, err := cim.ListControllerInstances(controllerType)
+	assert.Nil(t, err)
+	assert.NotNil(t, controllerInstanceMap1)
+	assert.Equal(t, 2, len(controllerInstanceMap1))
+
+	controllerInstanceMap2, err := cim.ListControllerInstances("foo2")
+	assert.Nil(t, err)
+	assert.NotNil(t, controllerInstanceMap2)
+	assert.Equal(t, 1, len(controllerInstanceMap2))
+}
---
+++ test/integration/cloudfabriccontrollers/framework_test.go
@@ -0,0 +1,164 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cloudfabriccontrollers
+
+import (
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"math"
+	"testing"
+	"time"
+)
+
+func TestMultipleReplicaSetControllerLifeCycle(t *testing.T) {
+	// case 1. start controller manager 1
+	s, closeFn1, cim1, rm1, informers1, client1 := rmSetup(t)
+	defer closeFn1()
+	stopCh1 := runControllerAndInformers(t, cim1, rm1, informers1, 0)
+	defer close(stopCh1)
+
+	// check replicaset controller status in controller manager 1
+	controllerInstanceList, err := client1.CoreV1().ControllerInstances().List(metav1.ListOptions{})
+	assert.Nil(t, err)
+	assert.NotNil(t, controllerInstanceList)
+	assert.Equal(t, 1, len(controllerInstanceList.Items), "number of controller instance")
+
+	rsControllerInstance1 := controllerInstanceList.Items[0]
+	assert.Equal(t, int64(math.MaxInt64), rsControllerInstance1.ControllerKey)
+	assert.NotNil(t, rsControllerInstance1.Name, "Nil controller instance name")
+	assert.False(t, rsControllerInstance1.IsLocked, "Unexpected 1st controller instance status")
+	assert.Equal(t, rm1.GetControllerType(), rsControllerInstance1.ControllerType, "Unexpected controller type")
+
+	// case 2. start controller manager 2
+	cim2, rm2, informers2, client2 := rmSetupControllerMaster(t, s)
+	stopCh2 := runControllerAndInformers(t, cim2, rm2, informers2, 0)
+	defer close(stopCh2)
+	time.Sleep(5 * time.Second)
+
+	// check replicaset controller status in controller manager 2
+	t.Logf("rm 1 instance id: %v", rm1.GetControllerName())
+	t.Logf("rm 2 instance id: %v", rm2.GetControllerName())
+
+	assert.NotEqual(t, rm1.GetControllerName(), rm2.GetControllerName())
+	controllerInstanceList2, err := client2.CoreV1().ControllerInstances().List(metav1.ListOptions{})
+	assert.Nil(t, err)
+	assert.NotNil(t, controllerInstanceList2)
+	assert.Equal(t, 2, len(controllerInstanceList2.Items), "number of controller instance")
+	t.Logf("new rms [%#v]", controllerInstanceList2)
+
+	rsControllerInstanceRead1, err := client2.CoreV1().ControllerInstances().Get(rm1.GetControllerName(), metav1.GetOptions{})
+	assert.Nil(t, err)
+	rsControllerInstanceRead2, err := client2.CoreV1().ControllerInstances().Get(rm2.GetControllerName(), metav1.GetOptions{})
+	assert.Nil(t, err)
+
+	// check controller instance updates
+	assert.Equal(t, rsControllerInstance1.Name, rsControllerInstanceRead1.Name)
+	assert.Equal(t, rsControllerInstance1.ControllerType, rsControllerInstanceRead1.ControllerType)
+	assert.Equal(t, int64(math.MaxInt64), rsControllerInstanceRead1.ControllerKey) // consistent hash
+	assert.Equal(t, rsControllerInstance1.ControllerKey, rsControllerInstanceRead1.ControllerKey)
+
+	assert.True(t, 0 < rsControllerInstanceRead1.ControllerKey)
+	assert.True(t, rsControllerInstanceRead2.ControllerKey < rsControllerInstanceRead1.ControllerKey)
+
+	assert.False(t, rsControllerInstanceRead1.IsLocked, "Unexpected 1st controller instance status")
+	assert.True(t, rsControllerInstanceRead2.IsLocked, "Unexpected 2nd controller instance status")
+	assert.Equal(t, rm2.GetControllerType(), rsControllerInstanceRead2.ControllerType, "Unexpected controller type")
+
+	// Controller Instance 1 release workloads
+	rm1.DoneProcessingCurrentWorkloads()
+	rsControllerInstanceRead2, err = client2.CoreV1().ControllerInstances().Get(rm2.GetControllerName(), metav1.GetOptions{})
+	assert.Nil(t, err)
+	assert.False(t, rsControllerInstanceRead2.IsLocked, "Unexpected 2nd controller instance status")
+
+	// case 3. start controller manager 3
+	cim3, rm3, informers3, client3 := rmSetupControllerMaster(t, s)
+	stopCh3 := runControllerAndInformers(t, cim3, rm3, informers3, 0)
+	defer close(stopCh3)
+	time.Sleep(5 * time.Second)
+	t.Logf("rm 3 instance id: %v", rm3.GetControllerName())
+
+	// check replicaset controller status in controller manager 2
+	assert.NotEqual(t, rm1.GetControllerName(), rm3.GetControllerName())
+	assert.NotEqual(t, rm2.GetControllerName(), rm3.GetControllerName())
+	controllerInstanceList3, err := client2.CoreV1().ControllerInstances().List(metav1.ListOptions{})
+	assert.Nil(t, err)
+	assert.NotNil(t, controllerInstanceList3)
+	assert.Equal(t, 3, len(controllerInstanceList3.Items), "number of controller instance")
+
+	rsControllerInstanceRead1, err = client3.CoreV1().ControllerInstances().Get(rm1.GetControllerName(), metav1.GetOptions{})
+	assert.Nil(t, err)
+	rsControllerInstanceRead2, err = client3.CoreV1().ControllerInstances().Get(rm2.GetControllerName(), metav1.GetOptions{})
+	assert.Nil(t, err)
+	rsControllerInstanceRead3, err := client3.CoreV1().ControllerInstances().Get(rm3.GetControllerName(), metav1.GetOptions{})
+	assert.Nil(t, err)
+
+	// check controller instance updates
+	assert.Equal(t, rsControllerInstance1.Name, rsControllerInstanceRead1.Name)
+	assert.Equal(t, rsControllerInstance1.ControllerType, rsControllerInstanceRead1.ControllerType)
+	assert.Equal(t, int64(math.MaxInt64), rsControllerInstanceRead1.ControllerKey) // consistent hash
+	assert.Equal(t, rsControllerInstance1.ControllerKey, rsControllerInstanceRead1.ControllerKey)
+
+	assert.True(t, 0 < rsControllerInstanceRead1.ControllerKey)
+	assert.True(t, rsControllerInstanceRead2.ControllerKey < rsControllerInstanceRead1.ControllerKey)
+	assert.True(t, rsControllerInstanceRead3.ControllerKey < rsControllerInstanceRead1.ControllerKey)
+	assert.NotEqual(t, rsControllerInstanceRead2.ControllerKey, rsControllerInstanceRead3.ControllerKey)
+
+	assert.False(t, rsControllerInstanceRead1.IsLocked, "Unexpected 1st controller instance status")
+	assert.False(t, rsControllerInstanceRead2.IsLocked, "Unexpected 2nd controller instance status")
+	assert.True(t, rsControllerInstanceRead3.IsLocked, "Unexpected 3rd controller instance status")
+	assert.Equal(t, rm2.GetControllerType(), rsControllerInstanceRead2.ControllerType, "Unexpected controller type")
+	assert.Equal(t, rm2.GetControllerType(), rsControllerInstanceRead3.ControllerType, "Unexpected controller type")
+	t.Logf("new rms [%#v]", controllerInstanceList3)
+
+	// controller instance 2 release workloads
+	rm2.DoneProcessingCurrentWorkloads()
+	time.Sleep(5 * time.Second)
+	rsControllerInstanceRead3, err = client3.CoreV1().ControllerInstances().Get(rm3.GetControllerName(), metav1.GetOptions{})
+	assert.Nil(t, err)
+	assert.False(t, rsControllerInstanceRead3.IsLocked, "Unexpected 3rd controller instance status")
+
+	/*
+		// case 4. 1st controller instance died - This needs to be done in unit test as integration test would be flaky
+		close(stopCh1)
+		// need to manually delete the controller instance from registry as default 5 minute timeout will cause test timeout during batch test.
+		err = client3.CoreV1().ControllerInstances().Delete(rm1.GetControllerName(), &metav1.DeleteOptions{})
+		assert.Nil(t, err)
+
+		controllerInstanceList4, err := client2.CoreV1().ControllerInstances().List(metav1.ListOptions{})
+		assert.Nil(t, err)
+		assert.NotNil(t, controllerInstanceList4)
+		assert.Equal(t, 2, len(controllerInstanceList4.Items))
+		t.Logf("new rms [%#v]", controllerInstanceList4)
+
+		rsControllerInstanceRead2, err = client2.CoreV1().ControllerInstances().Get(rm2.GetControllerName(), metav1.GetOptions{})
+		assert.Nil(t, err)
+		assert.NotNil(t, rsControllerInstanceRead2)
+		rsControllerInstanceRead3, err = client2.CoreV1().ControllerInstances().Get(rm3.GetControllerName(), metav1.GetOptions{})
+		assert.Nil(t, err)
+		assert.NotNil(t, rsControllerInstanceRead3)
+
+		// check replicaset controller status in controller manager 2
+		assert.NotEqual(t, rsControllerInstanceRead2.ControllerKey, rsControllerInstanceRead3.ControllerKey)
+		if rsControllerInstanceRead2.ControllerKey < rsControllerInstanceRead3.ControllerKey {
+			assert.Equal(t, int64(math.MaxInt64), rsControllerInstanceRead3.ControllerKey)
+		} else {
+			assert.Equal(t, int64(math.MaxInt64), rsControllerInstanceRead2.ControllerKey)
+		}
+		assert.False(t, rsControllerInstanceRead2.IsLocked, "Unexpected 2st controller instance status")
+		assert.False(t, rsControllerInstanceRead3.IsLocked, "Unexpected 3rd controller instance status")
+	*/
+}
